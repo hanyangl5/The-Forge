fe1db9573b9ce7ee4743ff6b7a5c2c49dffab2ba
Clang-Format <Clang-Format>
2024-12-07 06:06:44 +0000

    Clang-Format

--- Source/Runtime/Core/Public/Core/IThread.h
+++ Source/Runtime/Core/Public/Core/IThread.h
@@ -31,48 +31,43 @@
 
 #include <thread>
 
-namespace BD {
-    template<bool IsSharedMutex>
-    class Mutex{
-        public:
-            using MutexType = typename std::conditional<IsSharedMutex, std::shared_mutex, std::mutex>::type;
-        void Lock(){
-            return Mutex.lock();
-        }
-        void TryLock(){
-        return Mutex.try_lock();
-        }
-        void Unlock()
-        {
-            return Mutex.unlock();
-        }
-        void LockShared()
-        {
-            static_assert(IsSharedMutex);
-            return Mutex.lock_shared();
-        }
-        void TryLockShared()
-        {
-            static_assert(IsSharedMutex);
-            return Mutex.try_lock_shared();
-        }
-        void UnlockShared()
-        {
-            static_assert(IsSharedMutex);
-            return Mutex.unlock_shared();
-        }
-        private:
-            MutexType Mutex;
-    };
-    
-    template<>
-    class ScopeLock{
-        public:
+namespace BD
+{
+template<bool IsSharedMutex>
+class Mutex
+{
+public:
+    using MutexType = typename std::conditional<IsSharedMutex, std::shared_mutex, std::mutex>::type;
+    void Lock() { return Mutex.lock(); }
+    void TryLock() { return Mutex.try_lock(); }
+    void Unlock() { return Mutex.unlock(); }
+    void LockShared()
+    {
+        static_assert(IsSharedMutex);
+        return Mutex.lock_shared();
+    }
+    void TryLockShared()
+    {
+        static_assert(IsSharedMutex);
+        return Mutex.try_lock_shared();
+    }
+    void UnlockShared()
+    {
+        static_assert(IsSharedMutex);
+        return Mutex.unlock_shared();
+    }
 
-        private:
-    };
-}
+private:
+    MutexType Mutex;
+};
 
+template<>
+class ScopeLock
+{
+public:
+private:
+};
+} // namespace BD
 
 #if defined(_WINDOWS) || defined(XBOX)
 typedef unsigned long ThreadID;
